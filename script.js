321a7541c65ed0147acf6aacc270ced7e1c54c9c
Raounakvan <113039165+Raounakvan@users.noreply.github.com>
2025-04-03 21:17:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -207,7 +207,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-    window.onload = function() {
-        var shadowRoot = document.querySelector('spline-viewer').shadowRoot;
-        shadowRoot.querySelector('#logo').remove();
-    }
+   window.onload = function() {
+  var splineElement = document.querySelectorAll('spline-viewer');
+  
+  for (let pas = 0; pas < splineElement.length; pas++) {
+    var shadowRoot = splineElement[pas].shadowRoot;
+    shadowRoot.querySelector('#logo').remove();
+  }
+}
